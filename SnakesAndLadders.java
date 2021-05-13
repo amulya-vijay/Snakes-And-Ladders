85633e7df5ecb719e0f9d0af66e979ffdf8afd54
amulya-vijay <amulyakvijay297@gmail.com>
2021-05-13 17:12:26 +0530

    [Amulya] added play options

--- SnakesAndLadders.java
+++ SnakesAndLadders.java
@@ -4,5 +4,19 @@ public class SnakesAndLadders {
 	public static void main(String[] args) {
 		int player1=0;
 		int dice=(int) Math.floor(Math.random()*6+1);
+		int option=(int) Math.floor(Math.random()*2);
+		switch(option) {
+		case 0:
+			//Ladder-play
+			player1=player1+dice;
+			break;
+		case 1:
+			//Snake-play
+			player1=player1-dice;
+			break;
+		default:
+			//no-play
+			break;
+		}
 	}
 }
