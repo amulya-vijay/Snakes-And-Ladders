f58c200389a7a6a8ec5c542e5a58131a4e7c30df
amulya-vijay <amulyakvijay297@gmail.com>
2021-05-13 17:43:33 +0530

    [Amulya] added ouput of every roll of dice

--- SnakesAndLadders.java
+++ SnakesAndLadders.java
@@ -3,6 +3,7 @@ package com.snakesandladder;
 public class SnakesAndLadders {
 	public static void main(String[] args) {
 		int player1=0;
+		int rolls=0;
 		while(player1<100) {
 			int dice=(int) Math.floor(Math.random()*6+1);
 			int option=(int) Math.floor(Math.random()*2);
@@ -11,19 +12,27 @@ public class SnakesAndLadders {
 			case 0:
 				//Ladder-play
 				player1=player1+dice;
+				System.out.println(player1);
 				break;
 			case 1:
 				//Snake-play
 				player1=player1-dice;
+				System.out.println(player1);
 				break;
 			default:
 				//no-play
+				System.out.println(player1);
 				break;
+			}
+			if(player1<0) {
+				player1=0;
 			}
 			if(player1>100) {
 				player1=temp;
 			}
+			rolls++;
 		}
+		System.out.println("Number of rolls required to win the game="+rolls);
 		System.out.println(player1);
 	}
 }
