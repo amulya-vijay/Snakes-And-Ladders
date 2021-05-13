5d62c56363d2954a4c06efbf6e55cc663e1a2371
amulya-vijay <amulyakvijay297@gmail.com>
2021-05-13 17:01:18 +0530

    [Amulya] added die roll

--- SnakesAndLadders.java
+++ SnakesAndLadders.java
@@ -1,5 +1,8 @@
 package com.snakesandladder;
 
 public class SnakesAndLadders {
-
+	public static void main(String[] args) {
+		int player1=0;
+		int dice=(int) Math.floor(Math.random()*6+1);
+	}
 }
