ef78e410f04811b1d3051130b21972557ecbaecf
amulya-vijay <amulyakvijay297@gmail.com>
2021-05-13 17:27:19 +0530

    [Amulya] added winning position 100

--- SnakesAndLadders.java
+++ SnakesAndLadders.java
@@ -3,20 +3,23 @@ package com.snakesandladder;
 public class SnakesAndLadders {
 	public static void main(String[] args) {
 		int player1=0;
-		int dice=(int) Math.floor(Math.random()*6+1);
-		int option=(int) Math.floor(Math.random()*2);
-		switch(option) {
-		case 0:
-			//Ladder-play
-			player1=player1+dice;
-			break;
-		case 1:
-			//Snake-play
-			player1=player1-dice;
-			break;
-		default:
-			//no-play
-			break;
+		while(player1<100) {
+			int dice=(int) Math.floor(Math.random()*6+1);
+			int option=(int) Math.floor(Math.random()*2);
+			switch(option) {
+			case 0:
+				//Ladder-play
+				player1=player1+dice;
+				break;
+			case 1:
+				//Snake-play
+				player1=player1-dice;
+				break;
+			default:
+				//no-play
+				break;
+			}
 		}
+		System.out.println(player1);
 	}
 }
